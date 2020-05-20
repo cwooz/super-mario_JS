07c9304617abb6be2511d43cbdc94dc7a625b41b
cwooz <woosley.christopher@gmail.com>
2020-05-19 21:41:41 -0400

    basic level loaded

--- public/js/main.js
+++ public/js/main.js
@@ -1,64 +1,40 @@
-function loadImage(url) {
-  return new Promise(resolve => {
-    const image = new Image();
-    image.addEventListener('load', () => {
-      resolve(image);
-    });
-    image.src = url;
+import SpriteSheet from "./SpriteSheet.js";
+import { loadImage, loadLevel } from "./loaders.js";
+
+
+function drawBackgrond(background, context, sprites) {
+  background.ranges.forEach(([x1, x2, y1, y2]) => {
+
+    for (let x = x1; x < x2; x++) {
+      for (let y = y1; y < y2; y++) {
+        sprites.draw(background.tile, context, x, y);
+      }
+    }
   });
-}
-
-class SpriteSheet {
-  constructor(image, width, height) {
-    this.image = image;
-    this.width = width;
-    this.height = height;
-    this.tiles = new Map();
-  }
-
-  define(name, x, y) {
-    const buffer = document.createElement('canvas');
-    buffer.width = this.width;
-    buffer.height = this.height;
-    buffer
-      .getContext('2d')
-      .drawImage(
-        this.image,
-        x * this.width,
-        y * this.height,
-        this.width,
-        this.height,
-        0,
-        0,
-        this.width,
-        this.height
-      );
-    this.tiles.set(name, buffer);
-  }
-
-  draw(name, context, x, y) {
-    const buffer = this.tiles.get(name);
-    context.drawImage(buffer, x, y);
-  }
 }
 
 const canvas = document.getElementById('gameScreen');
 const context = canvas.getContext('2d');
 
-// context.fillRect(10, 10, 50, 50);
 // loadImage('./img/tiles.png')
 //   .then(image => {
 //     context.drawImage(image, 0, 0, 16, 16, 32, 32, 16, 16);
 //   });
 
-
 loadImage('./img/tiles.png')
   .then(image => {
     const sprites = new SpriteSheet(image, 16, 16);
     sprites.define('ground', 0, 0);
+    sprites.define('sky', 3, 23);
     sprites.draw('ground', context, 45, 62);
 
-
+    loadLevel('1-1')
+      .then(level => {
+        console.log(level);
+        level.background.forEach(background => {
+          drawBackgrond(background, context, sprites);
+        })
+      });
   });
 
 
